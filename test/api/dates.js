ca33a3bd275edf5960e8b739710b22fbafacd030
GazonkFoo <i9ru18j7>
2016-06-28 20:00:02 +0200

    make date test timezone independent

--- test/api/dates.js
+++ test/api/dates.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 
 describe('dates', function () {
     it('will be converted to ISO string', function (done) {
-        csvwriter({date:new Date(2000,1,2,3,4,5,6)}, function(err, csv) {
+        csvwriter({date:new Date('02 Feb 2000 02:04:05.006 GMT')}, function(err, csv) {
             expect(err).to.be.null;
             expect(csv).to.equal('date\r\n2000-02-02T02:04:05.006Z\r\n');
             done();
